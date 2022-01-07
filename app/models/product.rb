74699e82e4764477ecd8f288d8a6ee0e8d648168
shkuryn <shkuryn@gmail.com>
2022-01-07 23:52:07 +0300

    main coontroller output products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   validates :title, presence: true
   validates :price, presence: true
+  enum hit: { hit: 1, not_hit: 0 }
+  enum status: { active: 1, not_active: 0 }
 end
